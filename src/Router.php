3cba00217e0dd2a9515ecd4c434f75460f754b1e
Jeam Alves <jeanmalves@gmail.com>
2020-07-18 01:37:27 -0300

    Router class with parameter support

--- src/Router.php
+++ src/Router.php
@@ -3,7 +3,29 @@ namespace Source\Framework;
 
 class Router
 {
+    private array $routes = [];
+
     public function __construct()
     {
+    }
+
+    public function add(string $pattern, $callback)
+    {
+        $pattern = '/^' . str_replace('/', '\/', $pattern) . '$/';
+        $this->routes[$pattern] = $callback;
+    }
+
+    public function run()
+    {
+        $url = $_SERVER['REQUEST_URI'] ?? '/';
+
+        foreach ($this->routes as $route => $action) {
+
+            if (preg_match($route, $url, $params)) {
+                echo '<pre> '; var_dump($params); echo '</pre>';
+                return $action($params);
+            }
+        }
+        return 'Not found page';
     }
 }
