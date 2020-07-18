7cb4102fd94a88f5bdce657d8a5cd97b378c471b
Jeam Alves <jeanmalves@gmail.com>
2020-07-18 01:39:30 -0300

    Added app routes

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,18 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
-new \Source\Framework\Router;
+$router = new \Source\Framework\Router;
+
+$router->add('/', function () {
+    return 'home';
+});
+
+$router->add('/projects', function () {
+    return 'projects';
+});
+
+$router->add('/projects/(\d+)', function () {
+    return 'projects';
+});
+
+echo $router->run();
