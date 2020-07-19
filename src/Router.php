25eb0972e61e390849096f95aa88d1731ea20fe7
Jeam Alves <jeanmalves@gmail.com>
2020-07-19 01:10:12 -0300

    return array to execute response at bootstrap

--- src/Router.php
+++ src/Router.php
@@ -34,7 +34,7 @@ class Router
 
         foreach ($this->routes[$httpMethod] as $route => $action) {
             if (preg_match($route, $url, $params)) {
-                return $action($params);
+                return compact('action', 'params');
             }
         }
         throw new HttpException('Not found page', 404);
